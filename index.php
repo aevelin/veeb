f6972dd7882f4de147fa57c64b43e6643607a033
Evelin Arust <evelin.arust@khk.ee>
2017-12-08 09:27:46 +0200

    Kommentaaride kasutamise kirjeldus

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 8.12.2017
+ * Time: 9:03
+ */
+
+#see on ka üherealine kommentaar
+
+//üherealine kommentaar
+
+/**
+ * mitmerealine
+ * kommentaar
+ */
+#VCS-->Commit
+?>
+
+
