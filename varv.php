0fcd49076164f638c8b2060062bf162271e8ab32
Evelin Arust <evelin.arust@khk.ee>
2017-12-14 13:51:52 +0200

    Värvid muutuses

---
+++ varv.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 14.12.2017
+ * Time: 13:35
+ */
+for($rida = 1; $rida <= 5; $rida++) {
+    $varv = '#';
+     for($kord = 1; $kord <=6; $kord++) {
+         $juhuarv = rand(0, 15);
+         $symbol = dechex($juhuarv);
+         $varv = $varv.$symbol;
+     }
+        echo '<font color="'.$varv.'">'.
+            'Värviline tekst'.
+            '</font>'.
+            '<br />';
+}
+?>
