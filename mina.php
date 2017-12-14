4169afcf197ab4b9e3cd3331a6e36f0496938855
Evelin Arust <evelin.arust@khk.ee>
2017-12-14 13:25:22 +0200

    Nime defineerimine konstandiga

--- mina.php
+++ mina.php
@@ -5,6 +5,10 @@
  * Date: 14.12.2017
  * Time: 12:08
  */
-$myName = "Evelin";
-echo "Minu nimi on " . $myName;
+/*$myName = "Evelin";
+echo "Minu nimi on " . $myName;*/
+
+/*konstant; kui väärtused vaja hoida samana*/
+define('MINU_NIMI', 'Evelin');
+echo "Minu nimi on " . MINU_NIMI;
 ?>
