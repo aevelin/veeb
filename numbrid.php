6f2bf76c29a2fb64139988f6d6293bb71ce515fb
Evelin Arust <evelin.arust@khk.ee>
2017-12-14 13:37:42 +0200

    Värvilised numbrid

---
+++ numbrid.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 14.12.2017
+ * Time: 12:10
+ */
+$array = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+echo "<p style='color: red;'>$array[0], $array[2], $array[4], $array[6], $array[8]</p>";
+echo "<p style='color: blue;'>$array[1], $array[3], $array[5], $array[7], $array[9]</p>";
+?>
