2fd08d53957262f9886971caa2e783d3ac1eabec
Evelin Arust <evelin.arust@khk.ee>
2018-01-10 17:23:36 +0200

    Funktsioon elementideKorrutis

---
+++ elementideKorrutis.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * 4.
+ * Loo funktsioon nimega elementideKorrutis, mis
+ * võtab parameetrina täisarvude massiivi ja leiab
+ * korrutis nendest arvudest, mis on suurem kui 0
+ * ja mille indeksid on paarisarvud.
+ * Kontrollimiseks näiteks kasutada järgmine massiiv:
+ * 1, 0, 6, 0, 0, 3, 5
+ * Tulemus väljastatakse antud funktsiooni abil kujul:
+ * Tulemus: 1 * 6 * 5 = 30
+*/
+/**
+ * @param $massiivUus
+ * @return int
+ */
+
+function elementideKorrutis($array){
+    $tulemus = 1;
+    $korrutusMark = 0;
+    echo 'Tulemus: ';
+    for($i = 0; $i < count($array); $i++){
+        if($array[$i] > 0 and $i % 2 == 0){
+            $tulemus = $tulemus * $array[$i];
+            if($korrutusMark != 0){
+                echo ' * '.$array[$i];
+            } else {
+                echo $array[$i];
+                $korrutusMark = 1;
+            }
+        }
+    }
+    echo ' = '.$tulemus;
+}
+elementideKorrutis(array(1, 0, 6, 0, 0, 3, 5, 0, 2));
+?>
